1ba046619f30850e4f0bea7142d765e8b6e0de09
shtl00 <parmarsheetal@ymail.com>
2023-10-15 14:09:46 +0100

    first commit

---
+++ Practice.java
@@ -0,0 +1,10 @@
+public class Practice {
+    public static void main(String[] args) {
+        int a=1;
+        while(a<=10);
+        {
+            System.out.println("sheetal");
+            a++;
+
+    }
+}
