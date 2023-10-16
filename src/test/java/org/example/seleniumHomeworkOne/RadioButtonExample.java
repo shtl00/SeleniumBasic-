37d3c485d5f7c827e2d22d367cf35eb1b20fb17a
shtl00 <parmarsheetal@ymail.com>
2023-10-16 11:28:18 +0100

    first commit

--- src/test/java/org/example/seleniumHomeworkOne/RadioButtonExample.java
+++ src/test/java/org/example/seleniumHomeworkOne/RadioButtonExample.java
@@ -8,8 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RadioButtonExample {
-
-    public class RadioButtonExample{
+    public static class radioButtonExample {
 
         WebDriver driver;
         @Before
